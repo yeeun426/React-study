a6a5dcc8edb765ab76a1962a488de38730b64581
yeeun426 <thsudkcla7@naver.com>
2024-02-04 14:42:41 +0900

    feat: swiper 직접구현 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef } from "react";
+// prettier-ignore
 function App() {
+  const content1Ref = useRef<HTMLDivElement>(null);
+  const content2Ref = useRef<HTMLDivElement>(null);
+  const content3Ref = useRef<HTMLDivElement>(null);
+
+  const onContent1Click = () => {
+    content1Ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const onContent2Click = () => {
+    content2Ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const onContent3Click = () => {
+    content3Ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul
+        style={{
+          position: "fixed",
+          top: 0,
+          right: 0,
+          background: "teal",
+          padding: "20px",
+        }}
+      >
+        <li onClick={onContent1Click}>content1</li>
+        <li onClick={onContent2Click}>content2</li>
+        <li onClick={onContent3Click}>content3</li>
+      </ul>
+
+      <div ref={content1Ref} style={{ height: "500px", background: "skyblue" }}>
+        content1
+      </div>
+      <div ref={content2Ref} style={{ height: "500px", background: "orange" }}>
+        content2
+      </div>
+      <div ref={content3Ref} style={{ height: "500px", background: "gray" }}>
+        content3
+      </div>
     </div>
   );
 }
