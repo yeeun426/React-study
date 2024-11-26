1874ac7f34be04b2c9da3a5af013ae2d5e813951
yeeun426 <thsudkcla7@naver.com>
2024-11-26 21:37:43 +0900

    feat : redux 적용

--- src/vanila-redux/src/index.js
+++ src/vanila-redux/src/index.js
@@ -1,23 +1,27 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateNumber = () => {
-  number.innerText = count;
+const countModify = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
+  return count;
 };
 
-const handleAdd = () => {
-  count += 1;
-  updateNumber();
+const countStore = createStore(countModify);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
 
-const handleMinus = () => {
-  count -= 1;
-  updateNumber();
-};
+countStore.subscribe(onChange);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
