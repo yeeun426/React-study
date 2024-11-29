07c3db309a24b86ce444658712033c6931a40415
yeeun426 <thsudkcla7@naver.com>
2024-11-30 01:01:27 +0900

    feat : Detail (study mapStateToProps)

--- react-redux/src/router/Detail.js
+++ react-redux/src/router/Detail.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-function Detail() {
-  return <>엥</>;
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+  console.log(toDo);
+  return (
+    <>
+      <h1>DETAIL</h1>
+      <h2>{toDo.text}</h2>
+      <h5>Create at : {toDo.id}</h5>
+    </>
+  );
 }
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
