3fc376b049abc772d908932452e187633618a711
yeeun426 <thsudkcla7@naver.com>
2024-12-01 23:58:27 +0900

    install : redux toolkit

--- react-redux/src/router/Detail.js
+++ react-redux/src/router/Detail.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 function Detail({ toDos }) {
   const { id } = useParams();
   const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
-  console.log(toDo);
+
   return (
     <>
       <h1>DETAIL</h1>
