2d4f2bf8c5bb504fc73ce1f2e3131005a33a241f
yeeun426 <thsudkcla7@naver.com>
2024-12-02 17:06:19 +0900

    feat : createAction(redux-toolkit)

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,27 +1,35 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// redux 적용
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
+
+// redux-toolkit - createAction 적용
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+
+console.log(addToDo(), deleteToDo());
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
