d8137925e848561df01ec36d3e385c287b1b2ef3
yeeun426 <thsudkcla7@naver.com>
2024-12-02 17:30:15 +0900

    feat : createReducer(redux-toolkit)

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // redux 적용
 // const ADD = "ADD";
@@ -22,18 +22,27 @@ import { createAction } from "@reduxjs/toolkit";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-console.log(addToDo(), deleteToDo());
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
+// redux-toolkit - createReducer 적용
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.unshift({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) => {
       return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+    });
+});
 
 const store = createStore(reducer);
 
